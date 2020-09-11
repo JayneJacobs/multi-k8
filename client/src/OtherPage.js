15377b631d31b1998df247a06220be852ec31e7b
jjacob151 <jayne_jacobs@cable.comcast.com>
2020-09-11 11:32:19 -0400

    Adding a few more links

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -6,8 +6,6 @@ export default () => {
     <div>
       Im some other page
       <div><Link to="/">Go back to home page!</Link></div>
-      <div><Link to="http://jaynejacobs.com">Jayne's Website</Link></div>
-      <div><Link to="https://gopherclub.jaynejacobs.com/">Gopher Club</Link></div>
     </div>
   );
 };
